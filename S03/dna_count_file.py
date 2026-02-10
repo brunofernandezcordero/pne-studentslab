3cc2cbb1cf29b4ffdfb7c9c81b02735999e86aca
brunofernandezcordero <b.fernandezc.2025@alumnos.urjc.es>
2026-02-10 13:17:27 +0100

    Exercise 3 (SO3) I have used the 'with' function to directly closing the file after it is stored in a variable. (Optional: without using the close function)

---
+++ S03/dna_count_file.py
@@ -0,0 +1,15 @@
+with open('dna.txt') as f:
+    data = f.read().replace('\n','')
+def counting(data):
+    count_dict = {}
+    for i in data:
+        if i not in count_dict:
+            count_dict[i] = 1
+        else:
+            count_dict[i] += 1
+    print('Total length:', len(data))
+    for letter in count_dict:
+        print(f'{letter}: {count_dict[letter]}')
+    return
+
+counting(data)
