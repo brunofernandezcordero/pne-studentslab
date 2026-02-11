1aabc80d9caf4d4bf204fe974ddc15c78f857046
brunofernandezcordero <b.fernandezc.2025@alumnos.urjc.es>
2026-02-11 18:32:24 +0100

    exercise 5

---
+++ Basic Exercises/exercise6-functions.py
@@ -0,0 +1,23 @@
+print("--- Exercise 6a ---")
+def is_even(number):
+    even = number % 2 == 0
+    return even
+print("is_even(4):",is_even(4))
+print("is_even(7):",is_even(7))
+print("is_even(0):",is_even(0))
+print("is_even(-3):",is_even(-3))
+print("is_even(10):",is_even(10))
+
+print("--- Exercise 6b ---")
+def classify_triangle(a,b,c):
+    if a == b and b == c and c == a:
+        triangle = "equilateral"
+    elif a == b or b == c or c == a:
+        triangle = "isosceles"
+    else:
+        triangle = "scalene"
+    return triangle
+
+print("classify_triangle(5,5,5)",classify_triangle(5,5,5))
+print("classify_triangle(3,3,4)",classify_triangle(3,3,4))
+print("classify_triangle(3,4,5)",classify_triangle(3,4,5))
