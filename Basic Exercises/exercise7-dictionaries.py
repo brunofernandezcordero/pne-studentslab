a9d7e55f6b7dd0b3b753fa5f68cef096182594f8
brunofernandezcordero <b.fernandezc.2025@alumnos.urjc.es>
2026-02-11 18:43:39 +0100

    exercise 5

--- Basic Exercises/exercise7-dictionaries.py
+++ Basic Exercises/exercise7-dictionaries.py
@@ -4,3 +4,18 @@ student = {
     "subjects": ["PNE", "Networks", "Databases"],
     "grades": {"PNE": 8.5, "Networks": 7.0, "Databases": 9.2}
 }
+
+grades = student["grades"]
+count = 0
+for i in grades.values():
+    count += i
+    avg = count / len(grades)
+    avg = round(avg,2)
+
+print("Name:",student["name"])
+print("Number of subjects:", len(student["subjects"]))
+print("Enrolled in PNE:","PNE" in student["subjects"])
+print("Databeses grade:", student["grades"]["Databases"])
+print("Average grade:",avg)
+for i in grades.items():
+    print(f"{key}: {value}")
