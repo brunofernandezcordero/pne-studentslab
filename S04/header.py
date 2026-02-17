248cdaf56efd6ddd0e85cc8374cb566c45ec4af5
brunofernandezcordero <b.fernandezc.2025@alumnos.urjc.es>
2026-02-17 15:56:36 +0100

    exercise 5

--- S04/header.py
+++ S04/header.py
@@ -2,4 +2,7 @@ from pathlib import Path
 
 FILENAME = "sequences/RNU6_269P.txt"
 file_contents = Path(FILENAME).read_text()
-print(file_contents)
+for line in file_contents.splitlines():
+    if line.startswith(">"):
+        print(line)
+        break
