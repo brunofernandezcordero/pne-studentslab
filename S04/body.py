38d531ccf622cb9b5d613e6808663f04e691f29e
brunofernandezcordero <b.fernandezc.2025@alumnos.urjc.es>
2026-02-17 16:07:45 +0100

    exercise 5

---
+++ S04/body.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+FILENAME = "sequences/U5.txt"
+file_contents = Path(FILENAME).read_text()
+
+body = ""
+for line in file_contents.split("\n"):
+    if not line.startswith(">"):
+        body += line + "\n"
+
+print("Body of the U5.txt file:")
+print(body)
--- S04/header.py
+++ S04/header.py
@@ -2,7 +2,9 @@ from pathlib import Path
 
 FILENAME = "sequences/RNU6_269P.txt"
 file_contents = Path(FILENAME).read_text()
-for line in file_contents.splitlines():
+
+print("First line of the RNU6_269.txt file:")
+for line in file_contents.split("\n"):
     if line.startswith(">"):
         print(line)
         break
