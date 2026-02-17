d75b8aaced7e19c7329803813405ab676f81bd71
brunofernandezcordero <b.fernandezc.2025@alumnos.urjc.es>
2026-02-17 16:14:26 +0100

    exercise 5

---
+++ S04/sequence.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+FILENAME = "sequences/ADA.txt"
+file_contents = Path(FILENAME).read_text()
+
+body = ""
+for line in file_contents.split("\n"):
+    if not line.startswith(">"):
+        body += line
+
+print("The total number of bases in the ADA.txt head is:")
+print((len(body)))
