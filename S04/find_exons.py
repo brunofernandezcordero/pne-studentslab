22579b7b639549ef7937d1d30bba40b2550581f0
brunofernandezcordero <b.fernandezc.2025@alumnos.urjc.es>
2026-02-17 20:10:36 +0100

    exercise 7

--- S04/find_exons.py
+++ S04/find_exons.py
@@ -3,14 +3,47 @@ from pathlib import Path
 FILENAME = "sequences/ADA_EXONS.txt"
 file_contents = Path(FILENAME).read_text()
 
+FILENAME2 = "sequences/ADA.txt"
+complete_ADA = Path(FILENAME2).read_text()
+
+body = ""
+for line in complete_ADA.split("\n"):
+    if not line.startswith(">"):
+        body += line + "\n"
+
+complete_ADA = body
+
 exon_file_split = file_contents.split("\n")
 exons = []
+exon_base = ''
 for line in exon_file_split:
-    exon_base = ""
     if line.startswith(">"):
-        exons.append(exon_base)
+        if exon_base:
+            exons.append(exon_base)
+            exon_base = ''
         continue
     else:
-        exon_base+=line
-        # Aqui no se porque las bases  se me unen linea  alinea y no por exons
-print(exons)
+        exon_base += line
+
+if exon_base:
+    exons.append(exon_base)
+
+# Ahora tengo la lista exons con cada base sequence de cada exon en order
+
+max_cord = 44652852
+exon_number = 1
+print(f"{'Exon'}     | {'Long.'}     | {'Start'}     |{'End:'}")
+print('-'*50)
+for exon in exons:
+    start_index = complete_ADA.find(exon)
+    if start_index == -1:
+        print(f"Exon {exon_number} not found!")
+        continue # Me sale que ninguno es encontrado en el ADA compelto. Lo he comprobado manualmente y tampoco me encuentra los primeros exons
+    length = len(exon)
+
+    start_coord = max_cord - start_index
+    end_coord = start_coord - (length -1 )
+    print(f"{exon_number}     | {length}     | {start_coord}     | {end_coord}      ")
+    exon_number += 1
+
+
