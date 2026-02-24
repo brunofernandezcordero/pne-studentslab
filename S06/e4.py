fa453840c4e0e91ce6779deeadc0702eaa4a5c99
brunofernandezcordero <b.fernandezc.2025@alumnos.urjc.es>
2026-02-24 21:49:48 +0100

    no me functiona el color, preguntar, por termcolor y probar en ordenador de la uni

--- S06/e4.py
+++ S06/e4.py
@@ -1,4 +1,4 @@
-from termcolor import colored
+
 class Seq:
     """A class for representing sequences"""
 
@@ -19,17 +19,19 @@ def generate_seqs(pattern,n):
         seq_list.append(Seq(pattern * i ))
     return seq_list
 
-def print_seqs(seq_list,color):
-    for j,i in enumerate(seq_list):
+from termcolor import colored
+
+def print_seqs(seq_list, color):
+    for j, i in enumerate(seq_list):
         text = f"Sequence {j}: (Length: {i.len()}) {i}"
-        print(colored(text,color))
+        print(colored(text, color,attrs=['reverse']))
 
 seq_list1 = (generate_seqs("A", 3))
 seq_list2 = (generate_seqs("AC", 5))
 
 print("List 1:")
-print_seqs(seq_list1)
+print_seqs(seq_list1,'blue')
 
 print()
 print("List 2:")
-print_seqs(seq_list2)
+print_seqs(seq_list2,'red')
