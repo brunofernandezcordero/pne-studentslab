cfa0e1a8483469eef5f9ef48614300b02129a374
brunofernandezcordero <b.fernandezc.2025@alumnos.urjc.es>
2026-02-24 21:15:25 +0100

    exercise 7

--- S06/e3.py
+++ S06/e3.py
@@ -12,6 +12,11 @@ class Seq:
         """Calculate the length of the sequence"""
         return len(self.strbases)
 
+def generate_seqs(pattern,n):
+    seq_list = []
+    for i in range(1,n+1):
+        seq_list.append(Seq(pattern * i ))
+    return seq_list
 
 def print_seqs(seq_list):
     j = 0
@@ -19,5 +24,17 @@ def print_seqs(seq_list):
         print(f"Sequence {j}: (Length: {i.len()}) {i}")
         j += 1
 
-seq_list = [Seq("ACT"), Seq("GATA"), Seq("CAGATA")]
-print(print_seqs(seq_list))
+seq_list1 = (generate_seqs("A", 3))
+seq_list2 = (generate_seqs("AC", 5))
+
+print("List 1:")
+print_seqs(seq_list1)
+
+print()
+print("List 2:")
+print_seqs(seq_list2)
+
+
+
+
+
