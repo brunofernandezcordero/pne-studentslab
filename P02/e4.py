890f9e3e4cce13c614e4eb0ad8f3e5a2ad464449
brunofernandezcordero <b.fernandezc.2025@alumnos.urjc.es>
2026-03-04 17:58:59 +0100

    p02

---
+++ P02/e4.py
@@ -0,0 +1,22 @@
+from P01.Seq1 import Seq
+from Client0 import Client
+
+PRACTICE = 2
+EXERCISE = 4
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+IP = "212.128.255.98" # your IP address
+PORT = 8081
+
+# -- Create a client object
+c = Client(IP, PORT)
+s = Seq()
+genes = ["U5","ADA","FRAT1"]
+for gene in genes:
+    print(c)
+    gene_bases = Seq(s.read_fasta(gene))
+    importing = c.talk(f"Sending {gene} to the server...")
+    print(f"To Server: {importing}")
+    send_bases = c.talk(str(gene_bases))
+    print(f"To Server: {send_bases}")
--- P02/server(1-P02).py
+++ P02/server(1-P02).py
@@ -32,7 +32,7 @@ try:
         print("Message from client: {}".format(msg))
 
         # Send the message
-        message = "Hello from the teacher's server\n"
+        message = "From Server:\n"
         send_bytes = str.encode(message)
         # We must write bytes, not a string
         clientsocket.send(send_bytes)
