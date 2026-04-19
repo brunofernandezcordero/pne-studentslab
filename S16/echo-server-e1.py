71db9c995cbbefc691809fcd7988b34f40c9e33b
brunofernandezcordero <b.fernandezc.2025@alumnos.urjc.es>
2026-04-19 12:40:32 +0200

    final exercises practice 1

---
+++ S16/echo-server-e1.py
@@ -0,0 +1,81 @@
+import http.server
+import socketserver
+import termcolor
+from pathlib import Path
+from urllib.parse import parse_qs, urlparse
+
+# Define the Server's port
+PORT = 8080
+
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inherits all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+        url_path = urlparse(self.path)
+        path = url_path.path  # we get it from here
+        arguments = parse_qs(url_path.query)
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        if self.path == '/':
+            contents = Path('html/form-e1.html').read_text()
+            self.send_response(200)  # -- Status line: OK!
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(str.encode(contents)))
+            self.end_headers()
+            self.wfile.write(str.encode(contents))
+        elif self.path.startswith('/echo'):
+            message = arguments.get('msg',[''])[0]
+            contents = f"""
+            <html>
+                <body>
+                    <h1>Echo:</h1>
+                    <p>{message}</p>
+                    <a href='/'> Main page </a>
+                </body>
+            </html>
+            """
+
+            self.send_response(200)  # -- Status line: OK!
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(str.encode(contents)))
+            self.end_headers()
+            self.wfile.write(str.encode(contents))
+
+        else:
+            contents = Path('html/error.html').read_text()
+            self.send_response(200)  # -- Status line: OK!
+            self.send_header('Content-Type', 'text/html')
+            self.send_header('Content-Length', len(str.encode(contents)))
+            self.end_headers()
+            self.wfile.write(str.encode(contents))
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()
